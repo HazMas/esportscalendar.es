0b3c21d0cbeea8be1c179c1b0374468a7df6241f
Álvaro <alvaro.herrero.creativo@gmail.com>
2018-06-12 21:26:41 +0200

    feat(View): estructura base html

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ESportsCalendar msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ESportsCalendar from './components/ESportsCalendar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ESportsCalendar
   }
 }
 </script>
 
 <style lang="scss">
+$grid-breakpoints: (xs: 0, sm: 576px, md: 768px, lg: 992px, xl: 1200px) !default;
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Oswald', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  height: 100%;
+  background-image: linear-gradient(134deg, #424E75 0%, #262C4A 100%);;
+}
+body{
+  margin: 0;
+  height: 100%;
+}
+html{
+  height: 100%;
 }
 </style>
---
+++ src/components/ESportsCalendar.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="Landing-view">
+    <header class="esc-header">
+      <div class="logo-esports-calendar">
+        <img class="logo-esports-calendar__img" src="../assets/logo-esports-calendar.svg">
+        <h2 class="esc-title">
+          Todos tus E-sports
+        </h2>
+        <p>En una sola app</p>
+      </div>
+      <button>
+        playstore
+      </button>
+      <div>
+        indicador de scroll
+      </div>
+    </header>
+    <div class="esc-main">
+      <div class="esc-what-we-do">
+        <div class="esc-what-we-do__cards">
+          <div class="esc-what-we-do__card">
+            <img style="width:50px" class="logo-esports-calendar__img" src="../assets/icon-calendar.svg">
+            <p>Un calendario con todos los partidos</p>
+          </div>
+          <div class="esc-what-we-do__card">
+            <img style="width:50px" class="logo-esports-calendar__img" src="../assets/icon-clasification.svg">
+            <p>Las clasificaciones de tus ligas en directo</p>
+          </div>
+          <div class="esc-what-we-do__card">
+            <img style="width:50px" class="logo-esports-calendar__img" src="../assets/icon-notification.svg">
+            <p>Notificaciones para que no te pierdas nada</p>
+          </div>
+        </div>
+        <button>Prueba la app</button>
+      </div>
+      <div class="esc-competitions">
+        <h2>Sigue tus competiciones favoritas</h2>
+        <p>Todas las jornadas y todos los partidos ordenados y con clasificaciones en directo</p>
+        <div class="esc-competitions__shields">
+          <img style="width:100px" class="esc-competitions-shield" src="../assets/ligas/superliga-orange.png">
+          <img style="width:100px" class="esc-competitions-shield" src="../assets/ligas/esl-masters.png">
+          <img style="width:100px" class="esc-competitions-shield" src="../assets/ligas/copa-apertura.png">
+        </div>
+      </div>
+      <div class="esc-app-screens">
+        <div>
+          <h3>¡No te pierdas nada!</h3>
+          <h4>Sigue tus competiciones y equipos favoritos</h4>
+          <button>Prueba la app</button>
+        </div>
+        <img class="esc-app-screen" src="../assets/EsportsCalendar-app-screen-01.png">
+      </div>
+      <div class="esc-footer">
+        <p>Todas tus competiciones de esports favoritas, ordenadas y con resultados en directo.</p>
+        <img class="logo-esports-calendar__img" src="../assets/logo-esports-calendar.svg">
+        <a href="">Política de privacidad</a>
+        <a href="">términos de uso</a>
+        <p>Contacta</p>
+        <a href="">Twitter</a>
+        <a href="">Github</a>
+        <a href="">Youtube</a>
+        <a href="">Facebook</a>
+        <button>
+        playstore
+        </button>
+        <p>Con la colaboración de:</p>
+        <img src="../assets/ayto-gijon.svg">
+        <img src="../assets/gijon-impulsa.svg">
+        <a href="">Esportscalendar.app</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ESportsCalendar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.Landing-view {
+  height: 100%;
+}
+.esc-header {
+  height: 100%;
+  background-image: url(../assets/ESports-calendar-bg-header.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center 80%;
+  border-bottom: 5px solid white;
+}
+.logo-esports-calendar__img{
+  width:200px;
+  margin-top: 30px;
+}
+.esc-title{
+  font-size: 90px;
+  letter-spacing: 0;
+  text-align: center;
+  line-height: 80px;
+  text-transform: uppercase;
+  text-shadow: 0 0 30px rgba(255,255,255,0.87);
+}
+.esc-what-we-do {
+  background-image: url(../assets/ESportsCalendar-whatwedo.png);
+  background-position: 50% 50%;
+  background-size: cover;
+}
+.esc-what-we-do__cards {
+  display: flex;
+  justify-content: space-around;
+}
+.esc-competitions {
+  background-image: linear-gradient(180deg, #19213F 0%, #12152B 45%, #101328 100%);
+}
+.esc-app-screens {
+  background-image: url(../assets/ESportsCalendar-bg-lol.png);
+  background-position: center bottom;
+  background-size: cover;
+}
+.esc-footer {
+  background-image: url(../assets/ESportsCalendar-bg-footer.png);
+  background-position: center top;
+  background-size: cover;
+}
+</style>
